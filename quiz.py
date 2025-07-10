2b0c87bfff11cd1a0673e9aac9ceb6ed25f38a9a
Muhammad Abubakar Siddique Ansari <162357493+ansarionline@users.noreply.github.com>
2025-07-10 15:38:23 +0500

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -256,4 +256,5 @@ def admin():
         dialog.open()
     else:
         admin_page.visible = True
-ui.run(host='0.0.0.0', port = '8080')
+port = int(os.environ.get("PORT", 8080))
+ui.run(host="0.0.0.0", port=port)
